a98b5f2a85d1e7c21dd7e93b9e974b6140d0657e
Hsins <hsinspeng@gmail.com>
2020-05-01 05:04:19 +0800

    update.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,21 @@
 module.exports = {
   base: '/NTU-Thesis/',
-  title: 'NTU Thesis',
+  title: '國立臺灣大學論文模板',
   description: '',
   head: [
     ['link', { rel: 'stylesheet', href: "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.10.0/katex.min.css" }],
     ['link', { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css" }],
     ['link', { rel: "shortcut icon", type: "image/png", size: "any", href: "/favicon.png" }],
-    ['link', { rel: "icon", type: "image/svg+xml", size: "any", href: "/favicon.svg" }]
+    ['link', { rel: "icon", type: "image/svg+xml", size: "any", href: "/favicon.png" }]
   ],
 
   theme: 'antdocs',
   themeConfig: {
     logo: '/favicon.png',
     nav: [
-      { text: 'Home', link: '/' }
+      { text: 'Home', link: '/' },
+      { text: '使用指南', link: '/Guide/' },
+      { text: '模板下載', link: '/Download/' }
     ],
     lastUpdated: 'Last Updated',
     Host: 'https://hsins.github.io/',
